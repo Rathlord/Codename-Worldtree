0d24a5b23870a13a68ff2c4efe8eae6ccb08a5c5
Drake <editorion@gmail.com>
2018-08-09 19:54:19 -0400

    Initial Commit Project Setup Menu backdrop and a music player Some disfunctional UI elements

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        int numberOfMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        print("Number of music players = " + numberOfMusicPlayers);
+
+        if (numberOfMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
