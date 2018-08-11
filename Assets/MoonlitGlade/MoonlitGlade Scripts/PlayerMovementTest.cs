dc54a51b328a65d80a277381e460a472a033d305
Drake <editorion@gmail.com>
2018-08-11 17:50:11 -0400

    Wide changes- first scene test set up, player controller, gravity, drag, friction values (as well as I could so far...) on physics material.

--- Assets/MoonlitGlade/MoonlitGlade Scripts/PlayerMovementTest.cs
+++ Assets/MoonlitGlade/MoonlitGlade Scripts/PlayerMovementTest.cs
@@ -1,16 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovementTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody2D rigidBody;
+    float xThrow;
+    [SerializeField] [Tooltip("Player's speed tuning number")]public float speed = 10f;
+    [SerializeField] [Tooltip("Player's jump heigh number")]public float jumpHeight = 10f;
+
+    bool jumped = false;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.freezeRotation = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        HorizontalMovement();
+        Jumping();
 	}
+
+    private void Jumping()
+    {
+        bool jump = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (jump == true && jumped == false)
+        {
+            jumped = true;
+            rigidBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            jumped = false;
+        }
+    }
+
+    private void HorizontalMovement()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // Use horizontal cross-platform input
+
+        float horizontalMovement = xThrow * speed * Time.fixedDeltaTime; // set horizontal movement equal to horizontal throw * speed factor * time.deltatime to account for framerate
+
+        rigidBody.AddForce(Vector2.right * horizontalMovement);
+    }
+
+
 }
