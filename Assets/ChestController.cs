e5bc155f9c3ba99073ad482b5ac9363541caec7a
Drake <editorion@gmail.com>
2018-08-18 13:22:50 -0400

    .

---
+++ Assets/ChestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Object.Destroy(gameObject);
+    }
+
+}
