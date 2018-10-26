c9dcbb69060b0d66ebdd207acd989d8b80e7a967
Drake <editorion@gmail.com>
2018-10-26 16:31:25 -0400

    Chests must be opened with Interact button

--- Assets/CrossLevel/ChestController.cs
+++ Assets/CrossLevel/ChestController.cs
@@ -1,48 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class ChestController : MonoBehaviour
 {
-
+    public static ChestController instance;
     GameObject ItemTemplate;
     [SerializeField] Transform itemSpawnLocation;
     [SerializeField] Sprite openChest;
     SpriteRenderer spriteRenderer;
     BoxCollider2D boxCollider;
 
-    int IOnlyGiveOneItem = 1;
+    [SerializeField] bool inRangeOfChest = false;
 
     private void Awake()
     {
+        instance = this;
         spriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Interact"))
+        {
+            SpawnItem();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SpawnItemItem();
+            inRangeOfChest = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRangeOfChest = false;
+        }
+    }
+
+    public void SpawnItem()
+    {
+        if (inRangeOfChest == true)
+        {
+            print("I'm opening");
             spriteRenderer.sprite = openChest;
             boxCollider.enabled = false;
             SFXPlayer.instance.PlayChestOpen();
-        }
-
-    }
-
-    private void SpawnItemItem()
-    {
-        if (IOnlyGiveOneItem == 1)
-        {
-            IOnlyGiveOneItem = 0;
             ItemTemplate = ItemHolster.instance.RandomItemPicked();
             GameObject Item = Instantiate(ItemTemplate, transform.position, Quaternion.identity);
             print("I should be spawning an " + Item + " now");
-        }
-        else
-        {
-            return;
         }
     }
 }
